a77fdadaedb6bea86a1d8da8d3c8cf27e39d6fb4
decimalcoins <decimalcoins@gmail.com>
2025-09-13 12:10:04 +0700

    fix: update next.config dan PiContext

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co' }
-    ]
-  }
+      { protocol: 'https', hostname: 'placehold.co' },
+    ],
+  },
+
+  // 👇 Tambahan supaya env terbaca di sisi client
+  env: {
+    NEXT_PUBLIC_PI_ENV: process.env.NEXT_PUBLIC_PI_ENV,
+  },
 };
+
 module.exports = nextConfig;
