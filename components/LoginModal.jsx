09c37044f450149d8b125578366876a3d341f155
decimalcoins <decimalcoins@gmail.com>
2025-09-13 10:47:35 +0700

    fix: perbaikan LoginModal

--- components/LoginModal.jsx
+++ components/LoginModal.jsx
@@ -1,21 +1,22 @@
+
 "use client";
 import { useAppContext } from "@/context/PiContext";
 
 export default function LoginModal({ onClose }) {
-  const { handleUserLogin, handleAdminLogin } = useAppContext();
+  const { authenticate, upgradeToAdmin } = useAppContext();
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="bg-slate-800 p-6 rounded-lg w-96">
         <h2 className="text-xl font-bold mb-4">Masuk Sebagai</h2>
         <button
-          onClick={handleUserLogin}
+          onClick={authenticate}
           className="bg-blue-600 hover:bg-blue-700 text-white w-full py-2 rounded mb-3"
         >
           User
         </button>
         <button
-          onClick={handleAdminLogin}
+          onClick={upgradeToAdmin}
           className="bg-purple-600 hover:bg-purple-700 text-white w-full py-2 rounded"
         >
           Admin (Bayar 0.001 π)
